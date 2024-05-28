df49c9968bb4f5600b17c208b2785c446d9a1a61
CoolShivas <shivas2710cool00@gmailc.om>
2024-05-28 15:23:38 +0530

    Added the button switch toggler on Auth form

--- auth_ecommerce_website/src/components/auth/AuthForm.jsx
+++ auth_ecommerce_website/src/components/auth/AuthForm.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import styles from "./AuthForm.module.css";
 
 
 const AuthForm = () => {
 
+    const [isLogIn, setIsLogIn] = useState(true);
+
+    const switchModeToggler = () => {
+        setIsLogIn((prevState) => !prevState);
+    };
+
     return (
         <div className={styles.auth_form__div}>
             <div className={styles.auth_form__container}>
-                <h1> Auth Form is ready </h1>
+
+                <h1> {isLogIn ? "SignUp" : "Login"} </h1>
+                <button onClick={switchModeToggler}> {isLogIn ? "Create Account" : "Existing Account"} </button>
+
             </div>
         </div>
     )
