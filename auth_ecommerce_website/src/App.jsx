3d853990abcbdb5f84a6971697aa6ef3bfa03bc0
CoolShivas <shivas2710cool00@gmailc.om>
2024-05-28 11:58:15 +0530

    Starting auth ecommerce website

---
+++ auth_ecommerce_website/src/App.jsx
@@ -0,0 +1,11 @@
+import AppName from "./components/AppName";
+
+const App = () => {
+  return (
+    <>
+      <AppName></AppName>
+    </>
+  )
+}
+
+export default App;
---
+++ auth_ecommerce_website/src/components/AppName.jsx
@@ -0,0 +1,9 @@
+
+
+const AppName = () => {
+    return (
+        <h1> Auth Ecom </h1>
+    )
+}
+
+export default AppName;
