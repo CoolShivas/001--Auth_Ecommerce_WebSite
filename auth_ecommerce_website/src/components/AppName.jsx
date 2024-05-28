5d9b57c78bd8427ca03c4ed410ab39ccce482b8e
CoolShivas <shivas2710cool00@gmailc.om>
2024-05-28 12:19:32 +0530

    Addition of list with login logout and all

--- auth_ecommerce_website/src/components/AppName.jsx
+++ auth_ecommerce_website/src/components/AppName.jsx
@@ -2,7 +2,22 @@
 
 const AppName = () => {
     return (
-        <h1> Auth Ecom </h1>
+        <>
+            <nav className="navbar bg-primary" data-bs-theme="dark" style={{ color: "white" }}>
+                <h1> Auth Ecom </h1>
+                <div>
+                    <div>
+                        <ul>
+                            <li><a href="/"> home </a></li>
+                            <li><a href="/"> profile </a></li>
+                            <li><a href="/"> products </a></li>
+                            <li><a href="/"> login </a></li>
+                            <li><a href="/"> logout </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
 
