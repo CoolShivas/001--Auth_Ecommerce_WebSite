7137aea54b65d94f1b21c795140c17ebb04d35ef
CoolShivas <shivas2710cool00@gmailc.om>
2024-05-29 10:05:49 +0530

    Added Conditional Rendering to Navbar with the help of ContextApi

--- auth_ecommerce_website/src/components/header/Navbar.jsx
+++ auth_ecommerce_website/src/components/header/Navbar.jsx
@@ -5,7 +5,7 @@ import ContextApi from "../store/ContextApi";
 
 const Navbar = () => {
 
-    const { logout } = useContext(ContextApi);
+    const { logout, isUserLogIn } = useContext(ContextApi);
 
     const handlerOnLogOut = () => {
         console.log('logout')
@@ -16,25 +16,32 @@ const Navbar = () => {
     return (
         <div className={styles.navbar}>
             <ul className={styles.navbar_ultag}>
-                <li>
-                    <NavLink to="/home" className={styles.nav_nav}> home </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/profile" className={styles.nav_nav}> profile </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/product" className={styles.nav_nav}> products </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/" className={styles.nav_nav}> cart </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/auth" className={styles.nav_nav}> login </NavLink>
-                </li>
-                <li>
+                {isUserLogIn && <>
+
+                    <li>
+                        <NavLink to="/home" className={styles.nav_nav}> home </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/profile" className={styles.nav_nav}> profile </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/product" className={styles.nav_nav}> products </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/" className={styles.nav_nav}> cart </NavLink>
+                    </li>
+
+                </>}
+
+                {isUserLogIn && <li>
                     {/* <NavLink to="/" className={styles.nav_nav}> logout </NavLink> */}
                     <button className={styles.nav_nav} onClick={handlerOnLogOut}> Logout </button>
-                </li>
+                </li>}
+
+                {!isUserLogIn && <li>
+                    <NavLink to="/auth" className={styles.nav_nav}> login </NavLink>
+                </li>}
+
             </ul>
         </div>
     )
