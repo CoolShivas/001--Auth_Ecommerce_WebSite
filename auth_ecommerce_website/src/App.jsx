f3edda0b2c4894a3f413e6e3e52aeda6d3410d0a
CoolShivas <shivas2710cool00@gmailc.om>
2024-05-29 10:09:26 +0530

    Added Conditional Rendering to Route with the help of ContextApi

--- auth_ecommerce_website/src/App.jsx
+++ auth_ecommerce_website/src/App.jsx
@@ -4,17 +4,24 @@ import ProfilePage from "./components/pages/ProfilePage";
 import HomePage from "./components/pages/HomePage";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import AppName from "./components/header/AppName";
+import { useContext } from "react";
+import ContextApi from "./components/store/ContextApi";
 
 const App = () => {
+
+  const { isUserLogIn } = useContext(ContextApi);
+
   return (
     <>
       <AppName></AppName>
 
-      <Route path="/home">  <HomePage></HomePage> </Route>
-      <Route path="/profile"> <ProfilePage></ProfilePage> </Route>
-      <Route path="/product"> <ProductPage></ProductPage> </Route>
+      {isUserLogIn && <>
+        <Route path="/home">  <HomePage></HomePage> </Route>
+        <Route path="/profile"> <ProfilePage></ProfilePage> </Route>
+        <Route path="/product"> <ProductPage></ProductPage> </Route>
+      </>}
 
-      <Route path="/auth"> <AuthForm></AuthForm> </Route>
+      {!isUserLogIn && <Route path="/auth"> <AuthForm></AuthForm> </Route>}
     </>
   )
 }
