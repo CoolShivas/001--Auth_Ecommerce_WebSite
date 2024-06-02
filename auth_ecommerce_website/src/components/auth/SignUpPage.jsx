5652ed1e9d3b9da4bd3b6c69c41bef49649b893a
CoolShivas <shivas2710cool00@gmail.com>
2024-06-02 21:14:40 +0530

    SignUp functionality working with the use of firebase rest api and axios.

--- auth_ecommerce_website/src/components/auth/SignUpPage.jsx
+++ auth_ecommerce_website/src/components/auth/SignUpPage.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import styles from "./SignUpPage.module.css";
 
@@ -9,16 +10,40 @@ const SignUpPage = () => {
         redirectToLogin.replace("/login");
     };
 
-    const handlerOnSubmitForm = (event) => {
-        event.preventDefault();
+    const handlerOnSubmitForm = async (event) => {
 
-        const signUpData = {
-            Email: event.target.email.value,
-            Password: event.target.password.value,
-            ConfirmPassword: event.target.confirmpassword.value,
-        };
+        try {
+            event.preventDefault();
 
-        console.log(signUpData);
+            const signUpDetails = {
+                Email: event.target.email.value,
+                Password: event.target.password.value,
+                ConfirmPassword: event.target.confirmpassword.value,
+            };
+
+            console.log(signUpDetails);
+
+            if (signUpDetails.Password !== signUpDetails.ConfirmPassword) {
+                console.log("Password not matched");
+                return; // Exit the function if passwords don't match
+            }
+
+            const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD1lOMdT2N9SYAGevw979kq67kvXyFqUFc`, {
+                email: signUpDetails.Email,
+                password: signUpDetails.Password,
+                returnSecureToken: true
+            })
+
+
+            console.log('Response:', response.data)
+            console.log("SignUp Successfully");
+
+
+
+        } catch (error) {
+            console.log(error, "Something went wrong");
+        }
+
 
         event.target.email.value = "";
         event.target.password.value = "";
