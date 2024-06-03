fb79f0622c3bbf0fea04ab681006258d5d8b6f45
CoolShivas <shivas2710cool00@gmail.com>
2024-06-03 11:05:49 +0530

    Stitching done in ProfilePage for Changing User Password.

--- auth_ecommerce_website/src/components/pages/ProfilePage.jsx
+++ auth_ecommerce_website/src/components/pages/ProfilePage.jsx
@@ -1,11 +1,49 @@
 import styles from "./ProfilePage.module.css";
 
 const ProfilePage = () => {
+
+    const handlerOnChangePassProfile = (event) => {
+        event.preventDefault();
+
+        const changePassDetails = {
+            pass: event.target.changepass.value,
+            conpass: event.target.changeconfirmpass.value,
+        };
+
+        console.log(changePassDetails);
+    };
+
     return (
         <div className={styles.profile_page__div}>
-            <h1> This is a Profile Page. </h1>
+            <h1> Change User Profile Password. </h1>
+            <form className={styles.form_profile} onSubmit={handlerOnChangePassProfile}>
+                {/* <label> Change Your Password </label> */}
+                <div className="first_pass">
+                    <input
+                        type="password"
+                        id="changepass"
+                        placeholder="enter your new password"
+                        className="form-control"
+                        required
+                    />
+                </div>
+
+                <div className="second_pass">
+                    <input
+                        type="password"
+                        id="changeconfirmpass"
+                        placeholder="confirm your new password"
+                        className="form-control"
+                        required
+                    />
+                </div>
+
+                <div className={styles.actions_profile}>
+                    <button type="submit"> Change Password </button>
+                </div>
+            </form>
         </div>
-    )
-}
+    );
+};
 
 export default ProfilePage;
