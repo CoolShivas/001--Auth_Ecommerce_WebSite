d8eb7b15fc7d8af671977e8bc7f89f78b90a91d8
CoolShivas <shivas2710cool00@gmail.com>
2024-06-03 10:41:26 +0530

    Use of exact keyword inside Route with the help of Switch to implement

--- auth_ecommerce_website/src/App.jsx
+++ auth_ecommerce_website/src/App.jsx
@@ -1,10 +1,11 @@
+import WelComePage from "./components/pages/WelComePage";
 import LogInPage from "./components/auth/LogInPage";
 import SignUpPage from "./components/auth/SignUpPage";
 import Bucket from "./components/pages/cart/Bucket";
 import ProductPage from "./components/pages/ProductPage";
 import ProfilePage from "./components/pages/ProfilePage";
 import HomePage from "./components/pages/HomePage";
-import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import AppName from "./components/header/AppName";
 import { useContext } from "react";
 import ContextApi from "./components/store/ContextApi";
@@ -17,20 +18,24 @@ const App = () => {
     <>
       <AppName></AppName>
 
-      {isUserLogIn && <>
-        <Route path="/profile"> <ProfilePage></ProfilePage> </Route>
-        <Route path="/product"> <ProductPage></ProductPage> </Route>
-        <Route path="/cart"> <Bucket></Bucket> </Route>
-      </>}
-      {/* If the user is LOGIN then router available to access are the above mentioned only; */}
+      <Switch>
+        <Route exact path="/"> <WelComePage></WelComePage> </Route>
 
-      {!isUserLogIn && <>
-        <Route path="/home">  <HomePage></HomePage> </Route>
-        <Route path="/signup"> <SignUpPage></SignUpPage> </Route>
-        <Route path="/login"> <LogInPage></LogInPage> </Route>
-      </>}
-      {/* If the user is LOGOUT then router available to access are the above mentioned only; */}
+        {isUserLogIn && <>
+          <Route path="/profile"> <ProfilePage></ProfilePage> </Route>
+          <Route path="/product"> <ProductPage></ProductPage> </Route>
+          <Route path="/cart"> <Bucket></Bucket> </Route>
+        </>}
+        {/* If the user is LOGIN then router available to access are the above mentioned only; */}
 
+        {!isUserLogIn && <>
+          <Route path="/home">  <HomePage></HomePage> </Route>
+          <Route path="/signup"> <SignUpPage></SignUpPage> </Route>
+          <Route path="/login"> <LogInPage></LogInPage> </Route>
+        </>}
+        {/* If the user is LOGOUT then router available to access are the above mentioned only; */}
+
+      </Switch>
 
 
     </>
---
+++ auth_ecommerce_website/src/components/pages/WelComePage.jsx
@@ -0,0 +1,15 @@
+import styles from "./WelComePage.module.css";
+
+const WelComePage = () => {
+    return (
+        <div className={styles.welcome_page__div}>
+
+            <h2> Hi </h2>
+            <h2> Hello </h2>
+            <h1> Ram Ram </h1>
+
+        </div>
+    )
+}
+
+export default WelComePage;
